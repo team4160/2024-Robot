24f0b6d9605361cbc233c64514d1b370718cb635
rakosi2 <rattila5@hotmail.com>
2024-03-27 00:20:10 -0700

    Try autoaim

--- src/main/java/frc/robot/commands/Shooter/Shoot.java
+++ src/main/java/frc/robot/commands/Shooter/Shoot.java
@@ -1,26 +1,39 @@
 package frc.robot.commands.Shooter;
 
+import org.photonvision.PhotonCamera;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Swerve;
 
 public class Shoot extends Command{
     private final Intake intake;
     private final Indexer indexer;
     private final Shooter shooter;
+    private final Swerve swerve;
     private final Timer timer = new Timer();
     double velocity;
 
-    public Shoot(Intake intake, Indexer indexer, Shooter shooter, double velocity) {
+    PhotonCamera camera = new PhotonCamera("YOUR_CAMERA_NAME_HERE");
+    final double ANGULAR_P = 0.1;
+    final double ANGULAR_D = 0.0;
+    PIDController turnController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
+
+    public Shoot(Intake intake, Indexer indexer, Shooter shooter, Swerve swerve, double velocity) {
         this.intake = intake;
         this.indexer = indexer;
         this.shooter = shooter;
+        this.swerve = swerve;
         addRequirements(intake);
         addRequirements(indexer);
         addRequirements(shooter);
+        addRequirements(swerve);
         this.velocity = velocity;
     }
 
@@ -36,6 +49,18 @@ public class Shoot extends Command{
 
     @Override
     public void execute() {
+        var result = camera.getLatestResult();
+        double rotationSpeed;
+        if (result.hasTargets()) {
+            // Calculate angular turn power
+            // -1.0 required to ensure positive PID controller effort _increases_ yaw
+            //TODO: check if shooter AprilTag is visible
+            rotationSpeed = -turnController.calculate(result.getBestTarget().getYaw(), 0);
+        } else {
+            // If we have no targets, stay still.
+            rotationSpeed = 0;
+        }
+        swerve.drive(new Translation2d(), rotationSpeed, false, false);
         if (shooter.getVelocity() > velocity - 0.1) {
             SmartDashboard.putBoolean("Loaded", false);
             intake.setIntake(1);
